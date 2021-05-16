fbb7921daa5ab20b43b37286c4ee342631027f4c
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-05-16 17:12:54 +0530

    1 file modified

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ var s=document.getElementById("dino");
 function jump(e)
 {
 s.classList.add("Dino");
-setTimeout(()=>s.classList.remove("Dino"),2-00);
+setTimeout(()=>s.classList.remove("Dino"),400);
 }
 addEventListener("keydown",jump);
