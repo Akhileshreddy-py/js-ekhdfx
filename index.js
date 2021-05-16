c19ead7d91733837f728ed3e1ebed675365debf2
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-05-16 17:10:20 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,23 +1,10 @@
 // Import stylesheets
 import './style.css';
 
-// Write Javascript code!
-var j=0;
-var ak;
-var dino=document.getElementById("dino");
-var sub;
-function jump()
+var s=document.getElementById("dino");
+function jump(e)
 {
-sub=165-j;
-dino.style.top=sub+"px";
-j=j+1;
-if( j>=30)
-{
-j=-30;
-clearInterval(ak);
-dino.style.top=175+"px";
-ak=setInterval(jump,30)
+s.classList.add("Dino");
+setTimeout(()=>s.classList.remove("Dino"),500);
 }
-}
-ak=setInterval(jump,30);
-
+addEventListener("keydown",jump);
