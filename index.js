5144210930d849dff169a9e5aba5e86c42536996
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-05-16 18:06:51 +0530

    1 file modified

--- index.js
+++ index.js
@@ -2,24 +2,26 @@
 
 // Import stylesheets
 import './style.css';
+var d;
 var est;
 var enemy;
 var s=document.getElementById("dino");
-function jump(e)
+function jump(e) 
 {
 s.classList.add("Dino");
 setTimeout(()=>s.classList.remove("Dino"),400);
 }
 function detect()
-{var d=getComputedStyle(s).top;
+{var s=document.getElementById("dino");
+   var top=parseInt(getComputedStyle(s).top);
   enemy=document.getElementById("enemy");
 var est=parseInt(getComputedStyle(enemy).left);
-if(est<=10 && est>=0 && d<=10 && d>=0)
+if(est<=10 && est>=0 && top<=165 && top>=155 )
 { 
-document.getElementById("gameover").innerHTML=est;//  alert("game ")
+document.getElementById("gameover").innerHTML=est+" top :"+top;//  alert("game ")
 console.log("game over");
 }
 }
-setInterval(detect,10);
+setInterval(detect,1 );
 //detect();
 addEventListener("keydown",jump);
