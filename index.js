740bd989ecd4712ec9fd33ddc1022126162b3095
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-05-16 16:34:57 +0530

    1 file modified

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ import './style.css';
 
 // Write Javascript code!
 var j=0;
+var ak;
 var dino=document.getElementById("dino");
 var sub;
 function jump()
-{sub=175-j;
-  if(j<=25)
-  {
+{
+sub=165-j;
 dino.style.top=sub+"px";
-
-  }
+j=j+1;
+if( j>=30)
+{
+j=-30;
+clearInterval(ak);
+dino.style.top=175+"px";
+ak=setInterval(jump,30)
 }
-
-
+}
+ak=setInterval(jump,30);
 
