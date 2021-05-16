c61363dd845f2663e8aa8fc7e11ed117c2a680a1
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-05-16 16:15:55 +0530

    2 files modified

--- index.js
+++ index.js
@@ -2,3 +2,17 @@
 import './style.css';
 
 // Write Javascript code!
+var j=0;
+var dino=document.getElementById("dino");
+var sub;
+function jump()
+{sub=175-j;
+  if(j<=25)
+  {
+dino.style.top=sub+"px";
+
+  }
+}
+
+
+
