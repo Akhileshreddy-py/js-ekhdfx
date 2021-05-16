fb205e146a92cbccf2d88180af863cd8da7fef45
Akhileshreddy-py <reddyakhilesh94@gmail.com>
2021-05-16 17:24:14 +0530

    2 files modified

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
+
+
 // Import stylesheets
 import './style.css';
-
+var est;
+var enemy;
 var s=document.getElementById("dino");
 function jump(e)
 {
 s.classList.add("Dino");
 setTimeout(()=>s.classList.remove("Dino"),400);
 }
+function detect()
+{
+enemy=document.getElementById("enemy");
+var est=getComputedStyle(enemy).left;
+console.log(est);
+}
+
+detect();
 addEventListener("keydown",jump);
